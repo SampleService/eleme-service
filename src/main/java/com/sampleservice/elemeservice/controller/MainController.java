4cf382b4e9bda38a62afb45a899e2da6b8492963
SeptemberHX <september_hx@outlook.com>
2019-12-02 17:19:02 +0800

    init

---
+++ src/main/java/com/sampleservice/elemeservice/controller/MainController.java
@@ -0,0 +1,25 @@
+package com.sampleservice.elemeservice.controller;
+
+import com.septemberhx.common.base.MResponse;
+import com.septemberhx.mclient.annotation.MApiFunction;
+import com.septemberhx.mclient.annotation.MRestApiType;
+import com.septemberhx.mclient.base.MObject;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * @author SeptemberHX
+ * @version 0.1
+ * @date 2019/11/25
+ */
+@RestController
+public class MainController extends MObject {
+    @ResponseBody
+    @RequestMapping(value = "/carryout", method = RequestMethod.POST)
+    @MRestApiType
+    @MApiFunction
+    public MResponse carryoutFunction(@RequestBody MResponse requestData) {
+        MResponse result = new MResponse();
+        result.set("msg", "/carryout");
+        return result;
+    }
+}
