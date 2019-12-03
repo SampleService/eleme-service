9c167065c6f14ed475a5fe6fb8068f52e56d90d1
SeptemberHX <september_hx@outlook.com>
2019-12-03 16:53:43 +0800

    f

--- src/main/java/com/sampleservice/elemeservice/controller/MainController.java
+++ src/main/java/com/sampleservice/elemeservice/controller/MainController.java
@@ -1,5 +1,6 @@
 package com.sampleservice.elemeservice.controller;
 
+import com.sampleservice.elemeservice.utils.MBaseUtils;
 import com.septemberhx.common.base.MResponse;
 import com.septemberhx.mclient.annotation.MApiFunction;
 import com.septemberhx.mclient.annotation.MRestApiType;
@@ -20,6 +21,10 @@ public class MainController extends MObject {
     public MResponse carryoutFunction(@RequestBody MResponse requestData) {
         MResponse result = new MResponse();
         result.set("msg", "/carryout");
+
+        if (requestData.get("interval") != null) {
+            MBaseUtils.generateStringInKBSize(1000, result, (int) requestData.get("interval"));
+        }
         return result;
     }
 }
